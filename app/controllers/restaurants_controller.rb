281bd0ed0484d0be7fd6a39bfae76991ed8b95ce
Michael Bach <migobach@gmail.com>
2018-05-25 15:50:34 -0600

    add edit

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -21,6 +21,20 @@ class RestaurantsController < ApplicationController
     end
   end
 
+  def edit 
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurants_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def restaurant_params
